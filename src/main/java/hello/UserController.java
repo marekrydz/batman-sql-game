5260354b611078cff13bca3af296d933cfc7c77d
MarcoRy <rydzewski.marek86@gmail.com>
2019-10-01 22:58:27 +0200

    Created mainSubmit method which get string (sql query) from frontend from. Move code which is response for handle sql query, change to object list and send to welcome.html to this new mainSubmit method. Updated form to send request param

--- src/main/java/hello/UserController.java
+++ src/main/java/hello/UserController.java
@@ -2,17 +2,12 @@ package hello;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
-import javax.persistence.EntityManager;
-import java.lang.reflect.Array;
 import java.sql.*;
 import java.util.*;
 
@@ -32,59 +27,56 @@ public class UserController {
     private String message;
 
     @GetMapping("/")
-    public String main(Model model) throws SQLException {
+    public String main()  {
+
+      return "welcome";
+    }
+
+    @PostMapping("/")
+    public String mainSubmit(Model model, @RequestParam String sqlQuery)throws SQLException{
         String url = "jdbc:postgresql://localhost:5432/postgres";
         Connection conn = DriverManager.getConnection(url, "postgres", "marek");
-        String sql = "SELECT name FROM users ";
-        Map<String, List<String>> columnNameToValuesMap = new HashMap<>();
-        Multimap<String, String> map = ArrayListMultimap.create();
-        String key1 = "name";
-        String key2 = "email";
+        String sql = sqlQuery;
 
-        map.put(key1,"Marek");
-        map.put(key1,"Ania");
-        map.put(key2,"@1");
-        map.put(key2,"@2");
-        map.put(key1,"Marek");
-        map.put(key1,"Ania");
-        map.put(key2,"@1");
-        map.put(key2,"@2");
+        List<User> users = new ArrayList<>();
 
-        System.out.print(map.toString());
 
-System.out.print("Size get key" + map.keys().size());
-System.out.print("get key" + map.keySet());
         try {
+            Multimap<String, String> multiMap = ArrayListMultimap.create();
             Statement stmt = conn.createStatement();
             ResultSet rs;
             rs = stmt.executeQuery(sql);
             ResultSetMetaData resultSetMetaData = rs.getMetaData();
-            List<String> columnNames = new LinkedList<>();
-            int columnsNumber = resultSetMetaData.getColumnCount();
-
-            for(int i = 1; i<=columnsNumber; i++){
-                String columnName = resultSetMetaData.getColumnName(i);
-                columnNames.add(columnName);
-                columnNameToValuesMap.put(columnName, new ArrayList());
+            int columnCount = resultSetMetaData.getColumnCount();
+            List<String> s1 = new ArrayList<>();
+            for (int i = 1; i < columnCount + 1; i++) {
+                s1.add(resultSetMetaData.getColumnName(i));
+            }
+            while (rs.next()) {
+                for (int i = 0; i < s1.size(); i++) {
+                    multiMap.put(s1.get(i), rs.getString(s1.get(i)));
+                }
             }
 
-            while (rs.next()) {
-                for(String columnName: columnNames){
-                    List<String>columnDataList = columnNameToValuesMap.get(columnName);
+            List<String> names = new ArrayList<>(multiMap.get("name"));
+            List<String> emails = new ArrayList<>(multiMap.get("email"));
+            for (int i = 0; i < names.size(); i++) {
+                User multisetUser = new User(names.get(i), emails.get(i));
 
-                    columnDataList.add(rs.getString(columnName));
-                    columnNameToValuesMap.put(columnName,columnDataList);
-                }
+                users.add(multisetUser);
             }
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        System.out.print("1"+users.toString());
+
 
         model.addAttribute("message", message);
         model.addAttribute("tasks", tasks);
         model.addAttribute("users", repository.findAll());
-        model.addAttribute("answers",map.keySet());
+        model.addAttribute("users", users);
+
         return "welcome";
     }
 
