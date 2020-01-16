703908c6b1d24516103d04e8bac855d8091c6b48
MarcoRy <rydzewski.marek86@gmail.com>
2020-01-16 20:50:55 +0100

    AnswerRepository  - created String getHqlQueryFromGameLvl()method with using @Query with sql to get proper result

--- src/main/java/com/mr/game/repository/AnswerRepository.java
+++ src/main/java/com/mr/game/repository/AnswerRepository.java
@@ -2,10 +2,14 @@ package com.mr.game.repository;
 
 import com.mr.game.model.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import javax.annotation.Resource;
 
 @Resource
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
     Answer getAnswerByGameLvl(int gameLvl);
+
+    @Query(value = "select answer_hql from answers where id =?1", nativeQuery = true)
+    String getHqlQueryFromGameLvl(int gameLvl);
 }
--- src/test/java/com/mr/game/repository/DbRepositoryTest.java
+++ src/test/java/com/mr/game/repository/DbRepositoryTest.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Iterator;
+import java.util.List;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
+//@SpringBootTest
 @ContextConfiguration(classes = {DbRepository.class})
 public class DbRepositoryTest {
     @Autowired
@@ -31,5 +35,34 @@ public class DbRepositoryTest {
         assertTrue(result1);
         assertFalse(result2);
         assertTrue(result3);
+    }
+
+    @Test
+    public void getObjectsListUsingHqlQuery() {
+        String hqlQuery1 = "select h.name, h.realName from Hero h where h.name='Batman'";
+//        select h.name, bs.how, bs.place from heroes as h
+//join become_stories bs on h.become_story_id = bs.id
+//where h.name= 'Batman
+        String hqlQuery2 = "select h.name, bs.how, bs.place from Hero h join h.becomeStory bs where h.name='Batman'";
+
+//        Select * from heroes
+//full join  become_stories on heroes.become_story_id= become_stories.id
+        String hqlQuery3 = "select h.name, h.realName, bs.how, bs.place from Hero h full join h.becomeStory bs";
+//select * from heroes as h join weapons w on h.id = w.hero_id
+        String hqlQuery4 = "select h.name, h.realName, w.weaponName, w.characteristic from Hero h  join h.weapons w";
+        String hqlQuery5 = "select h.name, h.realName, e.nick, e.enemyRealName from Hero h left join h.enemies e";
+
+        List<Object> result1 = dbRepository.getObjectsListUsingHqlQuery(hqlQuery1);
+        List<Object> result2 = dbRepository.getObjectsListUsingHqlQuery(hqlQuery2);
+        List<Object> result3 = dbRepository.getObjectsListUsingHqlQuery(hqlQuery3);
+        List<Object> result4 = dbRepository.getObjectsListUsingHqlQuery(hqlQuery4);
+        List<Object> result5 = dbRepository.getObjectsListUsingHqlQuery(hqlQuery5);
+
+        Iterator it=result5.iterator();
+        while(it.hasNext())
+        {
+            Object rows[] = (Object[])it.next();
+            System.out.println(rows[0]+ " -- " +rows[1]+" -- " +rows[2]+" -- " +rows[3]);
+        }
     }
 }
