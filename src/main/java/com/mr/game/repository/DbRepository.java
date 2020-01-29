3b8ce65feab31d45c88e63ea40d9a0253658c95f
MarcoRy <rydzewski.marek86@gmail.com>
2020-01-29 20:10:57 +0100

    Updated connection to db

--- src/main/java/com/mr/game/repository/DbRepository.java
+++ src/main/java/com/mr/game/repository/DbRepository.java
@@ -3,8 +3,10 @@ package com.mr.game.repository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,10 +15,20 @@ import java.util.List;
 @Component
 public class DbRepository {
 
+    @Value("${spring.datasource.url}")
+    String dbUrl;
+    @Value("${spring.datasource.username}")
+    String userName;
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Resource
+    HibernateUtility hibernateUtility;
+
     private DbConnection dbConnection = new DbConnection();
 
     public boolean isResultOfSqlQueriesIdentical(String firstSqlQuery, String secondSqlQuery) {
-        Statement stmt = dbConnection.getConnectionToDb("jdbc:postgresql://localhost:5432/postgres", "postgres", "marek");
+        Statement stmt = dbConnection.getConnectionToDb(dbUrl, userName, password);
         ResultSet resultSet;
         boolean isIdentical = false;
         try {
@@ -30,7 +42,7 @@ public class DbRepository {
     }
 
     public List<Object> getObjectsListUsingHqlQuery(String hqlQuery){
-        SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
+        SessionFactory sessionFactory = hibernateUtility.getSessionFactory();
         Session session = sessionFactory.openSession();
         Query qry= session.createQuery(hqlQuery);
         return qry.list();
--- src/main/java/com/mr/game/repository/HibernateUtility.java
+++ src/main/java/com/mr/game/repository/HibernateUtility.java
@@ -3,24 +3,37 @@ package com.mr.game.repository;
 import com.mr.game.model.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class HibernateUtility {
+
     private static SessionFactory factory;
 
     private HibernateUtility() {
     }
 
-    public synchronized static SessionFactory getSessionFactory() {
+    @Value("${spring.datasource.url}")
+    String dbUrl;
+    @Value("${spring.datasource.username}")
+    String userName;
+    @Value("${spring.datasource.password}")
+    String password;
+
+
+    public SessionFactory getSessionFactory() {
+        System.out.println(dbUrl);
         if (factory == null) {
-            factory= new Configuration()
+            factory = new Configuration()
                     .addAnnotatedClass(Hero.class)
                     .addAnnotatedClass(Weapon.class)
                     .addAnnotatedClass(BecomeStory.class)
                     .addAnnotatedClass(Enemy.class)
                     .addAnnotatedClass(Answer.class)
-                    .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres")
-                    .setProperty("hibernate.connection.username", "postgres")
-                    .setProperty("hibernate.connection.password", "marek")
+                    .setProperty("hibernate.connection.url", dbUrl)
+                    .setProperty("hibernate.connection.username", userName)
+                    .setProperty("hibernate.connection.password", password)
                     .setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect")
                     .setProperty("hibernate.current_session_context_class", "thread")
                     .setProperty("hibernate.jdbc.lob.non_contextual_creation", "true")
